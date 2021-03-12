660769aed446c5db6755bba89c65ea8f5958d4b3
Rehan Haider <37621213+thesrs02@users.noreply.github.com>
2021-03-12 18:00:36 +0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Tailwindcss from "./Tailwind.svelte";
+</script>
+
+<Tailwindcss />
+
+<div class="bg-gray-200 h-full text-center flex text-center">
+  <div class=" w-7/12 bg-white p-12 m-auto rounded">
+    <h1 class="text-3xl font-light uppercase mb-4">Starter Project</h1>
+    <p class="font-light">
+      This is a basic project that integrates Tailwind Css with Rollup and Svelte.
+    </p>
+  </div>
+</div>
---
+++ src/Tailwind.svelte
@@ -0,0 +1,5 @@
+<style global type="text/postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
